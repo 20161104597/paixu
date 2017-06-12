24288a88dc81de0a1230d1f0b5f11ee99e4676c0
20161104597 <a20161104597@Mac15.local>
2017-06-12 11:54:31 +0800

    数字排序

--- paixu/main.c
+++ paixu/main.c
@@ -7,9 +7,31 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int i,j;
+    int a[10],t;
+    printf("请输入10个数字\n");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=1;j<10;j++)
+    {
+        for(i=0;i<10-j;i++)
+        {
+            if(a[i]<a[i+1])
+            {
+                t=a[i];
+                a[i]=a[i+1];
+                a[i+1]=t;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d ",a[i]);
+    }
     return 0;
 }
+
